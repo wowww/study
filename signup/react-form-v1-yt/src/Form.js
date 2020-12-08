bf391c0a6ffb915d63a19bd26a2af2d1bf7be207
Lia, Eunjee <58850132+wowww@users.noreply.github.com>
2020-12-08 22:43:08 +0900

    feat. form js 추가

---
+++ signup/react-form-v1-yt/src/Form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormSignup from './FormSignup'
+
+const Form = () => {
+  return (
+    <div>
+      <FormSignup></FormSignup>
+    </div>
+  )
+}
+
+export default Form
