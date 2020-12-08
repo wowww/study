677d5235f96920d4f22c857eae2fafdb251fd04a
Lia, Eunjee <58850132+wowww@users.noreply.github.com>
2020-12-08 23:35:05 +0900

    style. form css 추가

--- signup/react-form-v1-yt/src/FormSignup.js
+++ signup/react-form-v1-yt/src/FormSignup.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import useFrom from './useForm'
 import validate from './validateInfo'
+import './Form.css'
 
 const FormSignup = () => {
   const { handleChange, values, handleSubmit, errors } = useFrom(validate);
