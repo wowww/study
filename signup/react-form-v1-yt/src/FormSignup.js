8597dfd413a657093e226651a15588ae5c73354c
Lia, Eunjee <58850132+wowww@users.noreply.github.com>
2020-12-08 23:26:16 +0900

    feat. 예외처리 document에서 보이도록 p태그 추가

--- signup/react-form-v1-yt/src/FormSignup.js
+++ signup/react-form-v1-yt/src/FormSignup.js
@@ -3,7 +3,7 @@ import useFrom from './useForm'
 import validate from './validateInfo'
 
 const FormSignup = () => {
-  const { handleChange, values, handleSubmit } = useFrom(validate);
+  const { handleChange, values, handleSubmit, errors } = useFrom(validate);
 
   return (
     <div className="form-content-right">
@@ -22,6 +22,7 @@ const FormSignup = () => {
             value={values.username}
             onChange={handleChange}
             />
+            {errors.username && <p>{errors.username}</p>}
         </div>
         <div className="form-inputs">
           <label htmlFor="email" className="form-label">
