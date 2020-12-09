ecc043ccdf3ae4c9a1b618227dcb0f558fc6d450
Lia, Eunjee <58850132+wowww@users.noreply.github.com>
2020-12-09 17:38:08 +0900

    feat. login 기능 구현

--- react/signup/src/App.jsx
+++ react/signup/src/App.jsx
@@ -1,13 +1,55 @@
 import React from 'react';
 import './App.scss';
-import { Login } from './components/login/index';
+import { Login, Register } from './components/login/index';
 
-function App() {
-  return (
-    <div className="App">
-      <Login />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoginActive: true,
+    }
+  }
+
+  changeState() {
+    const { isLoginActive } = this.state;
+
+    if (isLoginActive) {
+      this.rightSide.classList.remove("right");
+      this.rightSide.classList.add("left");
+    } else {
+      this.rightSide.classList.remove("left");
+      this.rightSide.classList.add("right");
+    }
+
+    this.setState(preState => ({isLoginActive: !preState.isLoginActive}))
+  }
+
+  render() {
+    const { isLoginActive } = this.state;
+    const current = isLoginActive ? "Register" : "Login";
+    const currentActive = isLoginActive ? "login" : "register";
+
+    return (
+      <div className="App">
+        <div className="login">
+          <div className="container">
+            {isLoginActive && <Login containerRef={(ref) => this.current = ref}/> }
+            {!isLoginActive && <Register containerRef={(ref) => this.cuttent = ref}/>}
+          </div>
+          <RightSide current={current} containerRef={ref => this.rightSide = ref} onClick={this.changeState.bind(this)}/>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+const RightSide = props => {
+  return <div className="right-side" ref={props.containerRef} onClick={props.onClick}>
+    <div className="inner-container">
+      <div className="text">{props.current}</div>
     </div>
-  );
+    </div>
 }
 
 export default App;
