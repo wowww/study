b0120e35f205300a4742e1f72683c132e1d9747a
Lia, Eunjee <58850132+wowww@users.noreply.github.com>
2020-12-09 16:42:25 +0900

    docs. register

---
+++ react/signup/src/components/login/register.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import loginImg from '../../login.svg';
+
+export class Register extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="base-container">
+      <div className="header">Register</div>
+      <div className="content">
+        <div className="image">
+          <img src="{loginImg}" alt=""/>
+        </div>
+        <div className="form">
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" placeholder="Username" id=""/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" placeholder="Email" id=""/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" placeholder="Password" id=""/>
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+        <button type="button" className="btn">Register</button>
+      </div>
+    </div>
+  }
+}
