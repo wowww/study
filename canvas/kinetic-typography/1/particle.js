b9060d6ab4992f4d7e8e55738a44d102b192c44e
Kwon Eun Jee <passby_time@naver.com>
2020-12-15 01:40:27 +0900

    refactor. 오탈자 수정

--- canvas/kinetic-typography/1/particle.js
+++ canvas/kinetic-typography/1/particle.js
@@ -3,7 +3,7 @@ const MOVE_SPEED = 0.1;
 
 export class Particle {
   constructor(pos, texture) {
-    this.sprite = new devicePixelRatio.Sprite(texture);
+    this.sprite = new PIXI.Sprite(texture);
     this.sprite.scale.set(0.2);
     this.sprite.tint = 0x000000;
 
