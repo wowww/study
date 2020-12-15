f797a87314ffbb77bb13a3d75d59e57c961674c3
Kwon Eun Jee <passby_time@naver.com>
2020-12-16 02:04:32 +0900

    feat. jsonwebtoken

--- react/boiler-plate/models/User.js
+++ react/boiler-plate/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -40,17 +41,36 @@ userSchema.pre('save', function (next) {
       bcrypt.hash(user.password, salt, function (err, hash) {
         if (err) return next(err)
         user.password = hash
-        next();
+        next()
       })
-    });
-
+    })
+  } else {
     next()
   }
 
 })
 
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+  // plainPassword와 암호화된 password 같은지 비교를 위해서는  plainPassword를 암호화 시켜서 비교
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+    if(err) return cb(err),
+    cb(null, isMatch);
+  })  
+}
+
+userSchema.methods.generateToken = function(cb) {
+  var user = this;
+  
+  // jsonwebtoken을 이용해서 token 생성하기
+  var token = jwt.sign(user._id.toHexString(), 'secretToken');
+  user.token = token;
+  user.save(function(err, user) {
+    if(err) return cb(err)
+    cb(null, user)
+  })
+}
+
+
 const User = mongoose.model('User', userSchema)
 
-module.exports = {
-  User
-}
+module.exports = { User }
