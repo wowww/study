bcb78df2413f357088d96c4465f66bfa4bb46989
Kwon Eun Jee <passby_time@naver.com>
2020-12-15 16:34:48 +0900

    feat. Kinetic Typography
    리펙토링 필요

--- canvas/kinetic-typography/2/text.js
+++ canvas/kinetic-typography/2/text.js
@@ -1,11 +1,10 @@
 export class Text {
   constructor() {
     this.canvas = document.createElement('canvas');
-    this.canvas.style.position = 'absolute';
-    this.canvas.style.left = '0';
-    this.canvas.style.top = '0';
-  
-    document.body.appendChild(this.canvas);
+    // this.canvas.style.position = 'absolute';
+    // this.canvas.style.left = '0';
+    // this.canvas.style.top = '0';
+    // document.body.appendChild(this.canvas);
 
     this.ctx = this.canvas.getContext('2d');
   }
@@ -15,7 +14,7 @@ export class Text {
     this.canvas.height = stageHeight;
 
     const myText = str;
-    const fontWidth = 700;
+    const fontWidth = 1000;
     const fontSize = 800;
     const fontName = 'Hind';
 
