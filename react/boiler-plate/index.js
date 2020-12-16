e5ceb259ad91f7bc5c5de6ebbeed4b8c4c8f8520
Kwon Eun Jee <passby_time@naver.com>
2020-12-17 00:48:06 +0900

    feat. Auth 기능
    어떤 사이트를 들어갔을 때 여러가지 페이지
    로그인된 유저만 사용가능한 페이지, 관리자만 이용할 수 있는 페이지 등 체크해주기 위한 기능

--- react/boiler-plate/index.js
+++ react/boiler-plate/index.js
@@ -5,16 +5,12 @@ const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
-const config = require('./config/key')
-
-const {
-  User
-} = require('./models/User');
+const config = require('./config/key');
+const { auth } = require('./middleware/auth');
+const { User } = require('./models/User');
 
 // application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true}));
 
 // application/json
 app.use(bodyParser.json());
@@ -34,7 +30,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!~!!!')
 })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 
   // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
   // 그것들을 데이터 베이스에 넣어준다.
@@ -52,7 +48,7 @@ app.post('/register', (req, res) => {
   });
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
   // 요청된 이메일을 데이터 베이스에서 있는지 찾는다.
   User.findOne({ email: req.body.email}, (err, user) => {
     if (!user) { return res.json({ loginSuccess: false, message: "제공된 이메일에 해당하는 유저가 없습니다."}) }
@@ -73,8 +69,26 @@ app.post('/login', (req, res) => {
       })
     })
   })
-
 })
+
+
+// role 1 어드민, role 2 특정 부서 어드민
+// role 0 일반유저 role이 0이 아니면 관리자
+app.get('/api/users/auth'), auth, (req, res) => {
+  // 여기까지 미들웨어를 통과해 왔다는 이야기는 Authentication 이 true라는 말
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+}
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- react/boiler-plate/models/User.js
+++ react/boiler-plate/models/User.js
@@ -70,6 +70,18 @@ userSchema.methods.generateToken = function(cb) {
   })
 }
 
+userSchema.statics.findByToken = function(token, cb) {
+  var user = this;
+
+  // 토큰을 decode 한다.
+  jwt.verify(token, 'secretToken', function(err, decoded) {
+    // 유저 아이디를 이용해서 유저를 찾은 후, 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+    user.findOne({"_id": decoded, "token": token}, function(err, user) {
+      if(err) return cb(err)
+      cb(null, user)
+    })
+  })
+}
 
 const User = mongoose.model('User', userSchema)
 
