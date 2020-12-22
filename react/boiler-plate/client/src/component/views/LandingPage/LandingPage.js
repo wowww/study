abff0bc74c8bee3bd221ddffad1247f43aac608d
Kwon Eun Jee <passby_time@naver.com>
2020-12-23 05:28:01 +0900

    feat. 프록시 설치

--- react/boiler-plate/client/src/component/views/LandingPage/LandingPage.js
+++ react/boiler-plate/client/src/component/views/LandingPage/LandingPage.js
@@ -1,6 +1,14 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import axois from 'axios';
+import { response } from 'express';
 function LandingPage() {
+  
+  useEffect(() => {
+    axois.get('http://localhost:5000/api/hello')
+    .then(response => console.log(response.data))
+    
+  }, [])
+
   return (
     <div>
       LandingPage
--- react/boiler-plate/index.js
+++ react/boiler-plate/index.js
@@ -30,6 +30,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!~!!!')
 })
 
+app.get('/api/hello', (req, res) => {
+  res.send("안녕하세요~")
+})
+
 app.post('/api/users/register', (req, res) => {
 
   // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
