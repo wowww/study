81665c349e0bef7b11f63f426508264f14e4d55c
Kwon Eun Jee <passby_time@naver.com>
2020-12-24 22:06:41 +0900

    feat. 간단한 스타일링 적용 및 store, dispatch 생성

--- react/boiler-plate/client/src/component/views/LoginPage/LoginPage.js
+++ react/boiler-plate/client/src/component/views/LoginPage/LoginPage.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Axios from 'axios';
+import { useDispatch } from 'react-redux';
 
 function LoginPage() {
+  const dispatch = useDispatch();
+
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  }
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  }
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    let body = {
+      email: Email,
+      password: Password
+    }
+
+    dispatch(loginUser(body))
+  }
+
   return (
-    <div>
-      LoginPage
+    <div style={{ 
+      display:'flex', justifyContent:'center', alignItems:'center',
+      width:'100%', height:'100vh'}}
+      onSubmit={onSubmitHandler}
+    >
+      <form style={{display:'flex', flexDirection:'column'}}>
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        
+        <br />
+        <button type="submit">Login</button>
+      </form>
     </div>
   )
 }
