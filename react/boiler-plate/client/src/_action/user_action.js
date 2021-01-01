2902b4ca0ca6ad1415d5b7a633c634c95c971726
Kwon Eun Jee <passby_time@naver.com>
2021-01-01 20:17:17 +0900

    feat. boiler plate

--- react/boiler-plate/client/src/_action/user_action.js
+++ react/boiler-plate/client/src/_action/user_action.js
@@ -1,13 +1,11 @@
 import Axios from 'axios';
-import {
-  LOGIN_USER
-} from './types';
+import { LOGIN_USER } from './types';
 
 export function loginUser(dataTosubmit) {
   const request = Axios.post('/api/users/login', dataTosubmit)
         .then(response => response.data)
   return {
-    type: "LOGIN_USER",
+    type: LOGIN_USER,
     payload: request
   }
 }
--- react/boiler-plate/client/src/_reducers/user_reducer.js
+++ react/boiler-plate/client/src/_reducers/user_reducer.js
@@ -1,11 +1,9 @@
-import {
-  LOGIN_USER
-} from '../_action/types';
+import { LOGIN_USER } from '../_action/types';
 
 export default function (state = {}, action) {
   switch (action.type) {
     case LOGIN_USER:
-      return {...state, loginSuccess: action.payloag }
+      return {...state, loginSuccess: action.payload }
       break;
   
     default:
