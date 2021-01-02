791b7e2bfcee20d1d19ad27fab0e9c9e78a32acf
Kwon Eun Jee <passby_time@naver.com>
2021-01-02 22:39:16 +0900

    feat. RESISTER_USER 작성

--- react/boiler-plate/client/src/_action/user_action.js
+++ react/boiler-plate/client/src/_action/user_action.js
@@ -1,11 +1,20 @@
 import Axios from 'axios';
-import { LOGIN_USER } from './types';
+import { LOGIN_USER, RESISTER_USER } from './types';
 
 export function loginUser(dataTosubmit) {
   const request = Axios.post('/api/users/login', dataTosubmit)
         .then(response => response.data)
   return {
     type: LOGIN_USER,
+    payload: request
+  }
+}
+
+export function resisterUser(dataTosubmit) {
+  const request = Axios.post('/api/users/resister', dataTosubmit)
+        .then(response => response.data)
+  return {
+    type: RESISTER_USER,
     payload: request
   }
 }
--- react/boiler-plate/client/src/_reducers/user_reducer.js
+++ react/boiler-plate/client/src/_reducers/user_reducer.js
@@ -1,11 +1,13 @@
-import { LOGIN_USER } from '../_action/types';
+import { LOGIN_USER, RESISTER_USER } from '../_action/types';
 
 export default function (state = {}, action) {
   switch (action.type) {
     case LOGIN_USER:
       return {...state, loginSuccess: action.payload }
       break;
-  
+    case RESISTER_USER:
+      return {...state, resister: action.payload }
+      break;
     default:
       return state;
   }
