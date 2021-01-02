72b617b67544c5c2cd247c53abffb9069b9a7b77
Kwon Eun Jee <passby_time@naver.com>
2021-01-02 22:11:27 +0900

    feat. Resister page 작성(회원 가입)

--- react/boiler-plate/client/src/component/views/ResisterPage/ResisterPage.js
+++ react/boiler-plate/client/src/component/views/ResisterPage/ResisterPage.js
@@ -1,11 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../../_action/user_action'; 
 
-function ResisterPage() {
-  return (
-    <div>
-      ResisterPage !!
-    </div>
-  )
+function ResisterPage(props) {
+  const dispatch = useDispatch();
+
+  const [Email, setEmail] = useState("")
+  const [Name, setName] = useState("")
+  const [Password, setPassword] = useState("")
+  const [ConfirmPassword, setConfirmPassword] = useState("")
+
+
+  const onEmailHandler = (event) => {
+      setEmail(event.currentTarget.value)
+  }
+
+  const onNameHandler = (event) => {
+    setName(event.currentTarget.value)
+  }
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value)
+  }
+  const onConfirmPasswordHandler = (event) => {
+    setConfirmPassword(event.currentTarget.value)
+  }
+
+  const onSubmitHandler = (event) => {
+      event.preventDefault(); // 페이지 리프레시 방지
+
+      let body ={
+          email: Email,
+          password: Password
+      }
+
+      dispatch(loginUser(body))
+      .then(response => {
+          if (response.payload.loginSuccess) {
+              props.history.push("/")
+          } else {
+              alert('Error')
+          }
+      })
+    }
+
+    return (
+        <div style={
+            {display: 'flex', justifyContent: 'center', alignItems: 'center'
+            , width: '100%', height: "100vh"
+            }}>
+            <form style={{
+                display: 'flex', flexDirection:'column'
+            }} onSubmit={onSubmitHandler}>
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler} />
+
+                <label>Name</label>
+                <input type="text" value={Name} onChange={onNameHandler} />
+
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler} />
+                
+                <label>Confirm Password</label>
+                <input type="password" value={ConfirmPassword} onChange={onConfirmPasswordHandler} />
+
+                <br />
+                <button>
+                    회원가입
+                </button>
+            </form>
+        </div>
+    )
 }
 
 export default ResisterPage
