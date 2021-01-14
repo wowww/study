eda014e69fac98d0f4d00b41df6c5b013c88bee3
Kwon Eun Jee <passby_time@naver.com>
2021-01-15 07:42:19 +0900

    docs. About, Login / Register 추가

--- react/shopping-mall/src/components/Header/Header.js
+++ react/shopping-mall/src/components/Header/Header.js
@@ -32,6 +32,8 @@ export class Header extends Component {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/products">Products</Link></li>
             <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/login">Login / Register</Link></li>
             <li className="close">
               <img src={Close} alt="Close" onClick={this.menuToggle} />
             </li>
