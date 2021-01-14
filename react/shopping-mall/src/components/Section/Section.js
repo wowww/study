01e71d43f1b9fa62fb1021b9cc979a158746dffa
Kwon Eun Jee <passby_time@naver.com>
2021-01-15 07:37:06 +0900

    docs. Products, Details 페이지 Section에 추가 및 Router로 경로 지정

--- react/shopping-mall/src/components/Section/Section.js
+++ react/shopping-mall/src/components/Section/Section.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-
+import Products from './Products/Products';
+import Details from './Details/Details';
+import { Route, Router } from 'react-router-dom';
 import './Section.scss';
 
 export class Section extends Component {
   render() {
     return (
-      <div>
-        Section
-      </div>
+      <section>
+        <Route path="/product" component={Products} exact />
+        <Route path="/product/:id" component={Details} />
+      </section>
     )
   }
 }
