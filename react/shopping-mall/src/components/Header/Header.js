1037e8eb4b305aa699486bc8cfbd50128de4bfe1
Kwon Eun Jee <passby_time@naver.com>
2021-01-14 17:08:11 +0900

    style. sass 적용

--- react/shopping-mall/src/components/Header/Header.js
+++ react/shopping-mall/src/components/Header/Header.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Menu from '../svg/bars-solid.svg';
+import Close from '../svg/times-solid.svg';
+import CartIcon from '../svg/shopping-cart-solid.svg';
+import '../Header/Header.scss'
 
 export class Header extends Component {
   render() {
     return (
       <div>
-        Header
+        <div className="menu">
+          <img src={Menu} alt="Menu" />
+        </div>
       </div>
     )
   }
