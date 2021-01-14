2c96edcf53e9fcc12cda8eaa3018bda0773351ac
Kwon Eun Jee <passby_time@naver.com>
2021-01-15 07:35:53 +0900

    docs. Details, Products 추가

---
+++ react/shopping-mall/src/components/Section/Details/Details.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Details.scss'
+
+export class Details extends Component {
+  render() {
+    return (
+      <div>
+        Details
+      </div>
+    )
+  }
+}
+
+export default Details
---
+++ react/shopping-mall/src/components/Section/Products/Products.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Products.scss';
+
+export class Products extends Component {
+  render() {
+    return (
+      <div>
+        Products
+      </div>
+    )
+  }
+}
+
+export default Products
