9c5cb1e18d5862f9c2827c7bc07d82dc9fd28206
Kwon Eun Jee <passby_time@naver.com>
2021-01-16 21:28:02 +0900

    dosc. Details 페이지 작성

--- react/shopping-mall/src/components/Section/Details/Details.js
+++ react/shopping-mall/src/components/Section/Details/Details.js
@@ -1,12 +1,48 @@
-import React, { Component } from 'react'
-import './Details.scss'
+import React, { Component } from 'react';
+import { DataContext } from '../../Context/Context';
+import './Details.scss';
 
 export class Details extends Component {
+  static contextType = DataContext;
+  state = {
+    product: []
+  }
+
+  getProduct = () => {
+    if(this.props.match.params.id) {
+      const res = this.context.products;
+      const data = res.filter(item => {
+        return item._id === this.props.match.params.id
+      })
+      this.setState({product: data})
+    }
+  }
+
+  componentDidMount() {
+    this.getProduct();
+  }
+
   render() {
+    const { product } = this.state;
     return (
-      <div>
-        Details
-      </div>
+      <>
+        {
+          product.map(item => {
+            <div className="details" key={item._id}>
+              <img src={item.src} alt="" />
+              <div className="box">
+                <div className="row">
+                  <h2>{item.title}</h2>
+                  <span>${item.price}</span>
+                </div>
+                <p>{item.colors}</p>
+                <p>{item.Description}</p>
+                <p>{item.Content}</p>
+              </div>
+            </div>
+          })
+        }
+      </>
     )
   }
 }
