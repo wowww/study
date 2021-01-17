b85104ae03fbd75a58d812bd802d1e105ec1b8c3
Kwon Eun Jee <passby_time@naver.com>
2021-01-18 00:31:08 +0900

    docs. Cart에 아무것도 담기지 않았을 때 "Nothings Product" 문장 화면에 표시

--- react/shopping-mall/src/components/Section/Cart/Cart.js
+++ react/shopping-mall/src/components/Section/Cart/Cart.js
@@ -11,38 +11,44 @@ export class Cart extends Component {
   render() {
     const { cart, reduction, increase, removeProduct } = this.context;
 
-    return (
-      <>
-        {
-          cart.map(item => (
-            <div className="details cart" key={item._id}>
-              <img src={item.src} alt="" />
-              <div className="box">
-                <div className="row">
-                  <h2>{item.title}</h2>
-                  <span>${item.price}</span>
+    if (cart.length === 0) {
+      return (
+        <h2>Nothings Product</h2>
+      )
+    } else {
+      return (
+        <>
+          {
+            cart.map(item => (
+              <div className="details cart" key={item._id}>
+                <img src={item.src} alt="" />
+                <div className="box">
+                  <div className="row">
+                    <h2>{item.title}</h2>
+                    <span>${item.price}</span>
+                  </div>
+                  <Colors colors={item.colors} />
+                  <p>{item.Description}</p>
+                  <p>{item.Content}</p>
+                  <div className="amount">
+                    <button className="count" onClick={() => reduction(item._id)}> - </button>
+                    <span>{item.count}</span>
+                    <button className="count" onClick={() => increase(item._id)}> + </button>
+                  </div>
                 </div>
-                <Colors colors={item.colors} />
-                <p>{item.Description}</p>
-                <p>{item.Content}</p>
-                <div className="amount">
-                  <button className="count" onClick={() => reduction(item._id)}> - </button>
-                  <span>{item.count}</span>
-                  <button className="count" onClick={() => increase(item._id)}> + </button>
+                <div className="delete" onClick={() => removeProduct(item._id)}>
+                  X
                 </div>
               </div>
-              <div className="delete" onClick={() => removeProduct(item._id)}>
-                X
-              </div>
-            </div>
-          ))
-        }
-        <div className="total">
-          <Link to="/payment">Payment</Link>
-          <h3>Total: 0</h3>
-        </div>
-      </>
-    )
+            ))
+          }
+          <div className="total">
+            <Link to="/payment">Payment</Link>
+            <h3>Total: 0</h3>
+          </div>
+        </>
+      )
+    }
   }
 }
 
